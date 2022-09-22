67cb3a69800e5de5876c4947e0389c36fd211d23
Nirav Patel <niravpatelp129@gmail.com>
2022-09-22 19:41:05 -0400

    init

---
+++ src/app/chips/chips.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChipsComponent } from './chips.component';
+
+
+
+@NgModule({
+  declarations: [
+    ChipsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChipsModule { }
